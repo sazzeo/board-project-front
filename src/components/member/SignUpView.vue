c46a6212e47702bd3faa95337efe981912457809
sazzeo <imjee02@gmail.com>
2022-07-06 17:03:29 +0900

    sign-up view

--- src/components/member/SignUpView.vue
+++ src/components/member/SignUpView.vue
@@ -3,66 +3,60 @@
     <div class="sign-box">
       <div class="logo">BYULOG</div>
       <label>아이디</label>
-      <el-input
-          class="input-id input-box"
-          v-model="id"
-          input-box/>
+      <el-input class="input-id input-box" v-model="id" input-box />
       <label>비밀번호</label>
       <el-input
-          class="input-id input-box"
-          v-model="password"
-          type="password"
-          show-password
-          input-box/>
+        class="input-id input-box"
+        v-model="password"
+        type="password"
+        show-password
+        input-box
+      />
       <label>비밀번호 확인</label>
       <el-input
-          class="input-id input-box"
-          v-model="passwordCheck"
-          type="password"
-          show-password
-          input-box/>
+        class="input-id input-box"
+        v-model="passwordCheck"
+        type="password"
+        show-password
+        input-box
+      />
       <label>이름</label>
-      <el-input
-          class="input-id input-box"
-          v-model="name"
-          input-box/>
+      <el-input class="input-id input-box" v-model="name" input-box />
       <label>휴대폰번호</label>
-      <el-input
-          class="input-id input-box"
-          v-model="name"
-          input-box/>
+      <el-input class="input-id input-box" v-model="phone" input-box />
       <label>이메일</label>
-      <el-input
-          class="input-id input-box"
-          v-model="name"
-          input-box/>
+      <el-input class="input-id input-box" v-model="email" input-box />
       <label>주소</label>
-      <el-input
-          class="input-id input-box"
-          v-model="name"
-          input-box/>
-      <input type="text" v-model="postCode" placeholder="우편번호">
-      <input type="button" @click="showApi()" value="우편번호 찾기"><br>
-      <input type="text" v-model="addr" placeholder="주소"><br>
-      <input type="text" v-model="detailAddr" placeholder="상세주소">
-      <input type="text" v-model="extraAddr" placeholder="참고항목">
+      <input type="text" v-model="postCode" placeholder="우편번호" />
+      <input type="button" @click="showApi()" value="우편번호 찾기" /><br />
+      <input type="text" v-model="addr" placeholder="주소" /><br />
+      <input type="text" v-model="detailAddr" placeholder="상세주소" />
+      <input type="text" v-model="extraAddr" placeholder="참고항목" />
       <AddressDialog></AddressDialog>
     </div>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref } from "vue";
 
+const id = ref<string>();
+const password = ref<string>();
+const name = ref<string>();
+const phone = ref<string>();
+const email = ref<string>();
+const postCode = ref<number>();
+const addr = ref<string>();
+const detailAddr = ref<string>();
+const extraAddr = ref<string>();
 </script>
 
 <style scoped>
-
 .sing-up-wrapper {
   display: flex;
   justify-content: center;
   align-items: center;
 }
-
 
 .sign-box {
   width: 400px;
@@ -75,7 +69,6 @@
   flex-direction: column;
   align-items: center;
 }
-
 
 .sign-box > label {
   font-size: 13px;
@@ -92,7 +85,5 @@
 .input-box {
   height: 40px;
   font-size: 15px;
-
 }
-
 </style>
---
+++ src/types/member.ts
@@ -0,0 +1,11 @@
+export class Member {
+  id = "";
+  password = "";
+  name = "";
+  email = "";
+  phone = "";
+  postCode? = "";
+  addr? = "";
+  detailAddr? = "";
+  regDate? = "";
+}
