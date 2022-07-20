c039449149272b54150d9a5f682f882f03ddcdab
sazzeo <imjee02@gmail.com>
2022-07-20 23:48:03 +0900

    message box 추가

---
+++ src/components/message/MessageBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-button text @click="open">Click to open Message Box</el-button>
+</template>
+
+<script lang="ts" setup>
+import { ElMessageBox } from 'element-plus'
+
+const open = () => {
+  ElMessageBox.confirm(
+      'It will permanently delete the file. Continue?',
+      'Warning',
+      {
+        type: 'warning',
+      }
+  )
+}
+</script>
